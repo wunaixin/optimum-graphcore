ec6eb797e376c99e305b8e0ea25088d24e40b239
wunaixin <6475622@qq.com>
2023-03-30 00:13:30 +0800

    modified modeling_swin.py

--- examples/image-pretraining/run.sh
+++ examples/image-pretraining/run.sh
@@ -6,9 +6,8 @@ python run_mim.py \
     --remove_unused_columns False \
     --label_names bool_masked_pos \
     --do_train \
-    --do_eval \
     --learning_rate 2e-5 \
-    --num_train_epochs 5 \
+    --num_train_epochs 50 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --logging_strategy steps \
